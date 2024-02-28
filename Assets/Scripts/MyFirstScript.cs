724106e7f754137b84b696aacf225d9089261e2d
JackyWuCoder <JackyWuCoder@gmail.com>
2024-02-27 16:13:00 -0800

    Reviewed Basics of DataTypes

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    // int float string bool
+    int _wholeNumber = -5;
+    float _decimalNumber = 0.5f;
+    string _text = "$U#)U$O@J$O#@JsaD";
+    bool _isTrue = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _isTrue = false;
     }
 
     // Update is called once per frame
