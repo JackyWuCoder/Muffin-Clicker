9f66326f65cdb4a9aac226c2c921a3678f61e611
JackyWuCoder <JackyWuCoder@gmail.com>
2024-03-01 15:37:52 -0800

    Created a muffin clicked counter.

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    /*
     // int float string bool
     int _wholeNumber = -5;
     float _decimalNumber = 0.5f;
     string _text = "$U#)U$O@J$O#@JsaD";
     bool _isTrue = true;
+    */
 
+    private int _counter = 0;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         Debug.Log("Hello World");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
+
+    public void OnMuffinClicked() 
+    {
+        _counter = _counter + 1;
+        Debug.Log(_counter);
+    }
+
 }
