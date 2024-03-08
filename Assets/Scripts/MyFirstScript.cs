ab24926bb255381400038b64ba4bd4ebbbadbc7f
JackyWuCoder <JackyWuCoder@gmail.com>
2024-03-07 16:39:16 -0800

    Completed Assignments 1 and 2 for Muffin Clicker

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 using UnityEngine;
+using TMPro;
 
 public class MyFirstScript : MonoBehaviour
 {
@@ -13,16 +14,18 @@ public class MyFirstScript : MonoBehaviour
     bool _isTrue = true;
     */
 
-    Camera _camera;
-
+    [SerializeField]
+    private TextMeshProUGUI _totalMuffinsText;
     [SerializeField]
     private int _counter = 0;
-    [SerializeField] private int _counterPerClick = 1;
+    [SerializeField] private int _muffinsPerClick = 1;
+    [SerializeField] private float _critChance = 0.01f;
 
     // Start is called before the first frame update
     private void Start()
     {
         Debug.Log("Hello World");
+        UpdateTotalMuffins();
     }
 
     // Update is called once per frame
@@ -33,8 +36,29 @@ public class MyFirstScript : MonoBehaviour
 
     public void OnMuffinClicked() 
     {
-        _counter = _counter + _counterPerClick;
+        int _addedMuffins;
+        if (Random.Range(0.0f, 100.0f) <= _critChance*100)
+        {
+            _addedMuffins = _muffinsPerClick * 10;
+        }
+        else
+        {
+            _addedMuffins = _muffinsPerClick;
+        }
+        _counter = _counter + _addedMuffins;
+        UpdateTotalMuffins();
         Debug.Log(_counter);
     }
 
+    private void UpdateTotalMuffins() 
+    {
+        if (_counter == 1)
+        {
+            _totalMuffinsText.text = _counter.ToString() + " muffin";
+        } 
+        else 
+        {
+            _totalMuffinsText.text = _counter.ToString() + " muffins";
+        }
+    }
 }
--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeController : MonoBehaviour
 {
-    public void OnVolumeChanged() {
+    [SerializeField] private Slider _volumeSlider;
+    public void OnVolumeChanged() 
+    {
+        AudioListener.volume = _volumeSlider.value;
         Debug.Log("Volume Changed");
     }
 }
