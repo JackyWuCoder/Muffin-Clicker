127af242016b303a35d689891340edb64dd6f7fd
JackyWuCoder <JackyWuCoder@gmail.com>
2024-03-07 20:12:37 -0800

    Created a pulse Distance, Speed, and Offset

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -24,6 +24,10 @@ public class MyFirstScript : MonoBehaviour
     [SerializeField] private int _muffinsPerClick = 1;
     [SerializeField] private float _critChance = 0.01f;
 
+    [SerializeField] private float _pulseDistance = 1.0f;
+    [SerializeField] private float _pulseSpeed = 1.0f;
+    [SerializeField] private float _pulseOffset = 0;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -34,11 +38,22 @@ public class MyFirstScript : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        Debug.Log(Time.time);
+        Debug.Log(Mathf.Sin(Time.time));
         Vector3 rotation = new Vector3();
         rotation.z = _spinSpeed * Time.deltaTime;
         //transform.Rotate(rotation);
         _spinLight1.Rotate(rotation);
         _spinLight2.Rotate(rotation);
+        Vector3 pulse = new Vector3();
+        pulse.x = (_pulseDistance*Mathf.Sin(_pulseSpeed*Time.time) + _pulseOffset) * Time.deltaTime;
+        pulse.y = (_pulseDistance*Mathf.Sin(_pulseSpeed*Time.time) + _pulseOffset) * Time.deltaTime;
+        pulse.z = _spinLight1.transform.localScale.z;
+        _spinLight1.transform.localScale = pulse;
+        pulse.z = _spinLight2.transform.localScale.z;
+        _spinLight1.transform.localScale = pulse;
+
+
     }
 
     public void OnMuffinClicked() 
